1f1549de9a5510660c5976faef23d8fd3d40bf82
Bidur Gupta <bidurgupta125@gmail.com>
2024-09-28 02:26:59 +0530

    deploy

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "a43944e8f43b7a77db3723a300a46f79
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "272c9d8bcc876b159dcfe565e3dcf343",
+"flutter_bootstrap.js": "fdd89a15661b79d92c9cd7dd00e494ae",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
